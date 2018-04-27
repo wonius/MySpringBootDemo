f519c9e1c5421c8ce489b65191f8a3e29b1ac747
Gavin <woniuyf@163.com>
2018-04-27 15:17:52 +0800

    REST支持多媒体文件参数

--- src/main/java/com/springboot/controller/UserController.java
+++ src/main/java/com/springboot/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * 请编写注释
  *
@@ -22,5 +24,10 @@ public class UserController {
     @RequestMapping(value = "/getUser", method = RequestMethod.GET)
     public User getUser() {
         return userService.getUser();
+    }
+
+    @RequestMapping(value = "/multiFileTest", method = RequestMethod.POST)
+    public void multiFileTest(User user, HttpServletRequest request) {
+        System.out.println("success");
     }
 }
--- src/main/java/com/springboot/domain/User.java
+++ src/main/java/com/springboot/domain/User.java
@@ -1,14 +1,28 @@
 package com.springboot.domain;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.Serializable;
+
 /**
  * 请编写注释
  *
  * @author yangfan
  * @Date 2018/4/17
  */
-public class User {
+public class User implements Serializable {
     private String user;
     private String host;
+    private MultipartFile file;
+
+    public MultipartFile getFile() {
+        return file;
+    }
+
+    public void setFile(MultipartFile file) {
+        this.file = file;
+    }
+
     public String getUser() {
         return user;
     }
